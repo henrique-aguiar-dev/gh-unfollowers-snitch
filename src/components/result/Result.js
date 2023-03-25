a4a874974c832a53503d88f98b18201959ef3fbf
sos.lhti@gmail.com <sos.lhti@gmail.com>
2023-03-25 11:33:04 -0300

    Undo Testing routes

--- src/components/result/Result.js
+++ src/components/result/Result.js
@@ -92,7 +92,7 @@ const Result = () => {
 					<div className="list-container">
 						<Routes>
 							<Route
-								path="/dontfollow"
+								exact path="/dontfollow"
 								element={
 									<>
 										<h3>Don't follow back:</h3>
@@ -101,7 +101,7 @@ const Result = () => {
 								}
 							></Route>
 							<Route
-								path="/following"
+								exact path="/following"
 								element={
 									<>
 										<h3>Following:</h3>
@@ -110,7 +110,7 @@ const Result = () => {
 								}
 							></Route>
 							<Route
-								path="/followers"
+								exact path="/followers"
 								element={
 									<>
 										<h3>Followers:</h3>
@@ -119,7 +119,7 @@ const Result = () => {
 								}
 							></Route>
 							<Route
-								path="/mutual"
+								exact path="/mutual"
 								element={
 									<>
 										<h3>Mutual followers:</h3>

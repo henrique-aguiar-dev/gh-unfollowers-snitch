f24fbee691616b14e454c560238aeab53f7c1f96
sos.lhti@gmail.com <sos.lhti@gmail.com>
2023-03-25 16:04:14 -0300

    Dockerfile & no dotenv

--- src/services/axios/axios.js
+++ src/services/axios/axios.js
@@ -1,12 +1,11 @@
 import axios from "axios";
-import env from 'react-dotenv';
 
 const ghApi = axios.create({
 	baseURL: 'https://api.github.com/',
 })
 
 ghApi.interceptors.request.use(async config => {
-	if (env.REACT_APP_GHTOKEN) ghApi.defaults.headers.authorization = `Bearer ${env.REACT_APP_GHTOKEN}`;
+	if (process.env.REACT_APP_GHTOKEN) ghApi.defaults.headers.authorization = `Bearer ${process.env.REACT_APP_GHTOKEN}`;
 
   return config;
 });
